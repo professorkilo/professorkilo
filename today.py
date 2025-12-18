a9e9422401ef8898f879b65af0e7ee699bd45efa
professorkilo <23161081+professorkilo@users.noreply.github.com>
2025-12-18 14:12:03 -0500

    Refactor user data retrieval and timing logic

--- today.py
+++ today.py
@@ -5,34 +5,37 @@ if __name__ == '__main__':
     print('Calculation times:')
 
     # Look up your user data
-    user_data, user_time = perf_counter(user_getter, USER_NAME)
+    user_data = user_getter(USER_NAME)
+    user_time = 0.0
     OWNER_ID, acc_date = user_data
     formatter('account data', user_time)
 
-    # No age/birthday: just set a placeholder that will never be shown if your SVGs omit it
-    #age_data = ''
-    #age_time = 0.0
+    # No age/birthday
+    age_data = ''
+    age_time = 0.0
 
     # LOC and other stats
-    total_loc, loc_time = perf_counter(
-        loc_query,
-        ['OWNER', 'COLLABORATOR', 'ORGANIZATION_MEMBER'],
-        7
-    )
+    total_loc = loc_query(['OWNER', 'COLLABORATOR', 'ORGANIZATION_MEMBER'], 7)
+    loc_time = 0.0
     if total_loc[-1]:
         formatter('LOC (cached)', loc_time)
     else:
         formatter('LOC (no cache)', loc_time)
 
-    commit_data, commit_time = perf_counter(commit_counter, 7)
-    star_data, star_time = perf_counter(graph_repos_stars, 'stars', ['OWNER'])
-    repo_data, repo_time = perf_counter(graph_repos_stars, 'repos', ['OWNER'])
-    contrib_data, contrib_time = perf_counter(
-        graph_repos_stars,
-        'repos',
-        ['OWNER', 'COLLABORATOR', 'ORGANIZATION_MEMBER']
-    )
-    follower_data, follower_time = perf_counter(follower_getter, USER_NAME)
+    commit_data = commit_counter(7)
+    commit_time = 0.0
+
+    star_data = graph_repos_stars('stars', ['OWNER'])
+    star_time = 0.0
+
+    repo_data = graph_repos_stars('repos', ['OWNER'])
+    repo_time = 0.0
+
+    contrib_data = graph_repos_stars('repos', ['OWNER', 'COLLABORATOR', 'ORGANIZATION_MEMBER'])
+    contrib_time = 0.0
+
+    follower_data = follower_getter(USER_NAME)
+    follower_time = 0.0
 
     # Remove Andrew-specific deleted-repo archive logic
     # (no repository_archive.txt / special handling for a different user)
@@ -42,15 +45,14 @@ if __name__ == '__main__':
     #         total_loc[index] += archived_data[index]
     #     contrib_data += archived_data[-1]
     #     commit_data += int(archived_data[-2])
-
+ 
     # Format added, deleted, and total LOC
     for index in range(len(total_loc) - 1):
         total_loc[index] = '{:,}'.format(total_loc[index])
 
-    # Overwrite SVGs with your stats; if your SVGs have no age text, age_data will simply be unused
     svg_overwrite(
         'dark_mode.svg',
-        #age_data,
+        age_data,
         commit_data,
         star_data,
         repo_data,
@@ -60,7 +62,7 @@ if __name__ == '__main__':
     )
     svg_overwrite(
         'light_mode.svg',
-        #age_data,
+        age_data,
         commit_data,
         star_data,
         repo_data,
@@ -69,13 +71,12 @@ if __name__ == '__main__':
         total_loc[:-1],
     )
 
-    # Print total function time
     print(
         '\033[F\033[F\033[F\033[F\033[F\033[F\033[F\033[F',
         '{:<21}'.format('Total function time:'),
         '{:>11}'.format('%.4f' % (
             user_time
-            #+ age_time
+            + age_time
             + loc_time
             + commit_time
             + star_time
